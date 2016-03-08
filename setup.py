1cc1f1d248c1fe3edbc23a02c4d06a80e7c6794f
Daniel Schreij <dschreij@gmail.com>
2016-03-08 16:00:29 +0100

    ... And here is the setup.py script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='fileinspector',
+	version='1.0',
+	description='File inspector for Python',
+	author='Daniel Schreij',
+	author_email='dschreij@gmail.com',
+	url='https://github.com/dschreij/fileinspector',
+	py_modules=['fileinspector'],
+)
