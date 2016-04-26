608e7b0b3668f6bf591dc0089da8e7aa126bebfe
Daniel Schreij <dschreij@users.noreply.github.com>
2016-04-26 12:31:30 +0200

    Update fileinspector.py

--- fileinspector.py
+++ fileinspector.py
@@ -28,7 +28,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-__version__ = '0.1.0'
+__version__ = '1.0.1'
 
 import sys
 import mimetypes
